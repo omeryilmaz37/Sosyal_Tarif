7992187a18a314c0842b6f94bc5c73584da834b3
Ömer Yılmaz <omerryilmazz.37@gmail.com>
2025-05-04 15:44:35 +0300

    Initial Commit

---
+++ Sosyal_Tarif/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sosyal_Tarif
+//
+//  Created by Ömer Yılmaz on 4.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
